db48f8422eb429a47a731765503d8b453b5426ed
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-25 15:02:23 -0300

    react-router-dom added

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,9 +4,7 @@ import React from 'react'
 import Login from './pages/Login';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp'
-import NewNote from './pages/NewNote';
-import UpdateNote from './pages/UpdateNote'
-import Categories from './pages/Categories'
+
 
 
 export default function Routes() {
@@ -16,9 +14,6 @@ export default function Routes() {
           <Route path="/" exact component={Login} />
           <Route path="/user/signup" component={SignUp} />
           <Route path="/user/:userId" component={Home} />
-          <Route path="/:userId/categories" component={Categories} />
-          <Route path="/:userId/creation" component={NewNote} />
-          <Route path="/:noteId/update" component={UpdateNote} />
         </Switch>
       </BrowserRouter>
     );
