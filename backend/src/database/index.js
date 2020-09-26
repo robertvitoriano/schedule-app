f5ff0eb23162ef45dc0438c1f09c17b507671c92
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-26 08:29:08 -0300

    adding cors

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://robertvitoriano:123@cluster0.btwq6.mongodb.net/schedule-app?retryWrites=true&w=majority",()=>{
+mongoose.connect("mongodb+srv://robertvitoriano:123@cluster0.btwq6.mongodb.net/schedule-app?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true },()=>{
     console.log("I'm connected to mongodb")
 });
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const Router = require('./routes');
-
+const PORT = 4000;
 require('./database');
+app.use(cors())
 app.use(express.json());
 app.use(Router);
 
 app.listen(4000,()=>{
-    console.log('My app is running');
+    console.log("My app is up and running on port "+PORT);
 })
