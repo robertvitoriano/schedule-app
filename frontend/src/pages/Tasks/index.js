f62a6b3b88dcab073abd4f448b50accf42bb9035
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-26 11:17:15 -0300

    styling tasks page

--- frontend/src/pages/Tasks/index.js
+++ frontend/src/pages/Tasks/index.js
@@ -3,7 +3,7 @@ import api from './../../services/api'
 import Header from '../../components/Header';
 import './index.css'
 
-const  Tasks  =()=>{
+const  Tasks  =({history})=>{
 const [tasks,setTasks] = useState([]);
 useEffect(()=>{
     const lodTasks = async()=>{
@@ -17,14 +17,15 @@ useEffect(()=>{
 
 
     return(<div className="container tasks-container">
-             <Header/>
+             <Header history = {history}/>
              <h1 className="tasks-title">Tarefas Agendadas</h1>
              <div className="tasks-content">
+                 <div className="tasks-header"></div>
              <div className="tasks-scroll-list">
              <ul className="tasks-list">
             <div className="tasks-list-container">
                {tasks.map((task) => (
-                <li>{task.title}</li>
+                <li>{task.title}<a className="delete-button">X</a></li>
               ))} 
             </div>
           </ul>
