e4059fe4077f91ddcae85ad4bd4416b202e9962b
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-26 13:58:09 -0300

    creating update route

--- frontend/src/pages/Tasks/index.js
+++ frontend/src/pages/Tasks/index.js
@@ -1,59 +1,61 @@
-import React,{useState,useEffect} from 'react';
-import api from './../../services/api'
-import Header from '../../components/Header';
-import './index.css'
+import React, { useState, useEffect } from "react";
+import api from "./../../services/api";
+import Header from "../../components/Header";
+import "./index.css";
 
-const  Tasks  =({history})=>{
-const [tasks,setTasks] = useState([]);
-useEffect(()=>{
-    const lodTasks = async()=>{
-        const response = await api.get('/tasks');
-        setTasks(response.data);
-    }
-    lodTasks()
+const Tasks = ({ history }) => {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const lodTasks = async () => {
+      const response = await api.get("/tasks");
+      setTasks(response.data);
+    };
+    lodTasks();
+  }, [tasks]);
 
-},[tasks])
-
-const handleTaskDelete  = (e,id)=>{
+  const handleTaskDelete = (e, id) => {
     e.preventDefault();
 
-   fetch('http://localhost:4000/tasks', {
-    method: 'DELETE',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ id })
-}).then(response=>{
-    if(response.status ===200){
-        const  remainingTasks = tasks.filter((task)=>{
-            return task._i !== id
-        })
+    fetch("http://localhost:4000/tasks", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    }).then((response) => {
+      if (response.status === 200) {
+        const remainingTasks = tasks.filter((task) => {
+          return task._i !== id;
+        });
         setTasks(remainingTasks);
-    }
+      }
+    });
+  };
 
-})
-
-
-
-}
-
-
-    return(<div className="container tasks-container">
-             <Header history = {history}/>
-             <h1 className="tasks-title">Tarefas Agendadas</h1>
-             <div className="tasks-content">
-                 <div className="tasks-header"></div>
-             <div className="tasks-scroll-list">
-             <ul className="tasks-list">
+  return (
+    <div className="container tasks-container">
+      <Header history={history} />
+      <h1 className="tasks-title">Tarefas Agendadas</h1>
+      <div className="tasks-content">
+        <div className="tasks-header"></div>
+        <div className="tasks-scroll-list">
+          <ul className="tasks-list">
             <div className="tasks-list-container">
-               {tasks.map((task) => (
-                <li key={task._id}>{task.title}<a className="delete-button"  onClick= {e=>handleTaskDelete(e,task._id)}>X</a></li>
-              ))} 
+              {tasks.map((task) => (
+                <li key={task._id}>
+                  {task.title}
+                  <a
+                    className="delete-button"
+                    onClick={(e) => handleTaskDelete(e, task._id)}
+                  >
+                    Excluir
+                  </a>
+                </li>
+              ))}
             </div>
           </ul>
-          </div>
-          </div>
-    </div>)
-}
-
-
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Tasks;
